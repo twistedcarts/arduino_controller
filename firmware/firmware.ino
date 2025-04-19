c2d6ccbcb08a7022a8d717722e9333fc1821871d
Twisted <winkleytyler64@gmail.com>
2025-04-19 17:07:20 -0400

    Update firmware.ino

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -1,10 +1,9 @@
 #include <Mouse.h>
 
-bool active = false;
-
-// using mpx as default selection
-int recoil_Y = 6;
-int recoil_X = 1;
+bool is_active = false;
+int recoil_y = 6;
+int recoil_x = 1;
+String weapon_name = "MPX";
 
 void setup() {
     Serial.begin(9500);
@@ -13,36 +12,47 @@ void setup() {
 
 void loop() {
     if (Serial.available() > 0) {
-        char input = Serial.read();
+        String input = Serial.readStringUntil('\n');
 
-        // mouse i/o detection
-        if (input == '1') {
-            active = true;
-        } else if (input == '0') {
-            active = false;
-
-        // weapon recoil (modify these values by going into shooting range and tweaking with the values as the recoil pattern can change)
-        // since arduino's can only recieve bytes (or single characters) you can either convert a string, or just use a single letter/number for different weapons. the letters/numbers must match up with what is being sent in the python script
-        } else if (input == 'M') { // M4
-            recoil_Y = 15;
-            recoil_X = 2;
-        } else if (input == 'X') { // MPX
-            recoil_Y = 6; 
-            recoil_X = 1;
-        } else if (input == 'P') { // P90
-            recoil_Y = 18; 
-            recoil_X = 0;
-        } else if (input == 'F') { // F2
-            recoil_Y = 29; 
-            recoil_X = 1;
-        } else if (input == 'S') { // SMG-12
-            recoil_Y = 13;
-            recoil_X = 0;
-        } 
+        if (input == "1") {
+            is_active = true;
+        } else if (input == "0") {
+            is_active = false;
+        } else if (input.startsWith("CFG:")) {
+            parse_weapon_config(input.substring(4));
+        }
     }
 
-    if (active) {
-        Mouse.move(-recoil_Y, recoil_Y, 0);
-        delay(30);
+    if (is_active) {
+        smooth_move(recoil_x, recoil_y);
+    }
+}
+
+void smooth_move(int x, int y) {
+    const int steps = 3;
+    const int step_delay = 20 / steps;
+
+    static int step_index = 0;
+    static unsigned long last_step_time = 0;
+
+    if (millis() - last_step_time >= step_delay) {
+        if (step_index < steps) {
+            Mouse.move(x / steps, y / steps, 0);
+            step_index++;
+        } else {
+            step_index = 0;
+        }
+        last_step_time = millis();
+    }
+}
+
+void parse_weapon_config(String cfg) {
+    int first_comma = cfg.indexOf(',');
+    int second_comma = cfg.indexOf(',', first_comma + 1);
+
+    if (first_comma > 0 && second_comma > first_comma) {
+        weapon_name = cfg.substring(0, first_comma);
+        recoil_y = cfg.substring(first_comma + 1, second_comma).toInt();
+        recoil_x = cfg.substring(second_comma + 1).toInt();
     }
 }
